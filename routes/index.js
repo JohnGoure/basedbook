31d61abce92c89964752a710f336cbdf54b2530a
JohnGoure <goure@uiwtx.edu>
2018-03-02 16:41:53 -0600

    Changed routing to be in the through the root, index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/based');
-});
+// Require controller modules
+var user_controller = require('../controllers/userController');
+
+/// USER ROUTES ///
+
+// GET user home page
+router.get('/', user_controller.index);
+
+router.post('/', user_controller.user_create_post);
+
+// GET request for creating a User
+router.get('/create', user_controller.user_create_get);
+
+// POST request for creating a User
+router.post('/create', user_controller.user_create_post);
+
+// GET request for Deleting a User
+router.get('/:id/delete', user_controller.user_delete_get);
+
+// POST request for Deleting a User
+router.post('/:id/delete', user_controller.user_delete_post);
+
+// GET request for Updating a User
+router.get('/:id/update', user_controller.user_update_get);
+
+// POST request for Updating a User
+router.post('/:id/update', user_controller.user_update_post);
+
+// GET request for logged in user homepage
+router.get('/:id/', user_controller.user_logged_in);
+
+// GET request for logged in user settings
+router.get('/:id/settings', user_controller.user_settings);
 
 module.exports = router;

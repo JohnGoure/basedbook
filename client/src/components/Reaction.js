739ea08e8bff0bb7b0d24319ce1f708f82a842c2
JohnGoure <johnsgithub@gmail.com>
2018-04-09 22:21:31 -0500

    Refactored the component architecture. Still working on a good system for organization.

--- client/src/components/Reaction.js
+++ client/src/components/Reaction.js
@@ -4,6 +4,7 @@ import TimePosted from './TimePosted'
 export const Reaction = () => (
     <div className='Reaction'>
         <p>Like</p>
+        <p>Reply</p>
         <TimePosted />
     </div>
 )
